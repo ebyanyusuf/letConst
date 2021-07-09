b7a64a392701289559ea0d58527059bad69830c5
ebyanyusuf <ebyan.yusuf@gmail.com>
2021-07-09 09:20:15 -0400

    first

---
+++ letConst.js
@@ -0,0 +1,8 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = 2.6
+    let area = (Math.PI) * (r * r)
+    console.log(area)
+    
+    let perimeter = 2 * (Math.PI) * r
+    console.log(perimeter)
